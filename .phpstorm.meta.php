15ddc2d06f8d4ac89e2688ab645e6a1244c741fd
Michael Karbowiak <karbowiak@gmail.com>
2017-01-16 19:28:43 +0100

    Update composer to add mongodb, add phpstorm meta (that doesn't work..) and update index

---
+++ .phpstorm.meta.php
@@ -0,0 +1,17 @@
+<?php
+namespace PHPSTORM_META {
+    $STATIC_METHOD_TYPES = [
+        \Interop\Container\ContainerInterface::get('') => [
+            "" == "@",
+        ],
+        \DI\Container::get('') => [
+            "" == "@",
+        ],
+        \EasyMock\EasyMock::easyMock('') => [
+            "" == "@",
+        ],
+        \EasyMock\EasyMock::easySpy('') => [
+            "" == "@",
+        ]
+    ];
+}
