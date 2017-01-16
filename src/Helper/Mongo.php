ef6a87ab53ba64d2148d6e4f68e7c77e32de4d2b
Michael Karbowiak <karbowiak@gmail.com>
2017-01-16 23:24:17 +0100

    Update the mongo helper

--- src/Helper/Mongo.php
+++ src/Helper/Mongo.php
@@ -2,6 +2,33 @@
 
 namespace EVEAuth\Helper;
 
+use MongoDB\BSON\UTCDateTime;
+use MongoDB\Client;
+
 class Mongo {
+    public $collectionName = "";
+    public $databaseName = "eveauth";
+
+    protected $mongodb;
+    protected $collection;
+
+    public function __construct(Client $mongodb) {
+        $this->mongodb = $mongodb;
+        if(empty($this->databaseName))
+            throw new \Exception("Error, no database selected");
+
+        $this->collection = $mongodb->selectCollection($this->databaseName, $this->collectionName);
+    }
+
+    public function makeTimeFromDateTime($dateTime): UTCDateTime {
+        $unixTime = strtotime($dateTime);
+        $milliseconds = $unixTime * 1000;
+        return new UTCDatetime($milliseconds);
+    }
+
+    public function makeTimeFromUnixTime($unixTime): UTCDateTime {
+        $milliseconds = $unixTime * 1000;
+        return new UTCDatetime($milliseconds);
+    }
 
 }
