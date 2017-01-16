fec99b9a6f77783844187203927f31c810369494
Michael Karbowiak <karbowiak@gmail.com>
2017-01-16 19:29:21 +0100

    Add call definition to dependencies, and add libraries needed under src for stuff later on

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,23 +1,27 @@
 <?php
 // DIC configuration
-
 $container = $app->getContainer();
 
-// view renderer
-$container['view'] = function ($c) {
-    $settings = $c->get('settings');
-    $view = new Slim\Views\Twig($settings['view']['template_path'], $settings['view']['twig']);
+// View renderer
+$container["view"] = function (\Slim\Container $c) {
+    $settings = $c->get("settings");
+    $view = new Slim\Views\Twig($settings["view"]["template_path"], $settings["view"]["twig"]);
     // Add extensions
-    $view->addExtension(new Slim\Views\TwigExtension($c->get('router'), $c->get('request')->getUri()));
+    $view->addExtension(new Slim\Views\TwigExtension($c->get("router"), $c->get("request")->getUri()));
     $view->addExtension(new Twig_Extension_Debug());
     return $view;
 };
 
-// monolog
-$container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
+// Monolog
+$container["logger"] = function (\Slim\Container $c) {
+    $settings = $c->get("settings")["logger"];
+    $logger = new Monolog\Logger($settings["name"]);
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings["path"], $settings["level"]));
     return $logger;
+};
+
+// MongoDB
+$container["mongodb"] = function (\Slim\Container $c) {
+    return new MongoDB\Client("mongodb://127.0.0.1:27017", array(), array("typeMap" => array("root" => "array", "document" => "array", "array" => "array")));
 };
